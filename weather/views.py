17af5187e9512ecf091f310ad6ed18273f81c847
fredkwesiga <fredkwesiga9@gmail.com>
2024-05-14 12:22:16 +0300

    refactors the html and adds more logic to the views.py in the weather app

--- weather/views.py
+++ weather/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
+import json
+import urllib.request
 
 # Create your views here.
 def index(request):
     if request.method == 'POST':
-        city == request.POST['city']
+        city = request.POST['city']
+        res = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=f4a3510d070079b65e5cd6cc3538b8d2').read()
+        json_data = json.loads(res)
+        data = {
+            "country_code": str(json_data['sys']['country']),
+            "coordinate": str(json_data['coord']['lon']) + ' ' +
+            str(json_data['coord']['lat']),
+            "temp": str(json_data['main']['temp'])+'k',
+            "pressure": str(json_data['main']['pressure']),
+            "humidity": str(json_data['main']['humidity']),
+        }
     else:
         city = ''
-    return render(request, 'index.html', {'city': city})
+        data = {}
+    return render(request, 'index.html', {'city': city, 'data': data})
